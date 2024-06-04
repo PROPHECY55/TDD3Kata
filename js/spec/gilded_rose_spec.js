67a0dc295886e0a024fb5d50fb52a67cbe7ef7a0
Radic Ivo <89729414+PROPHECY55@users.noreply.github.com>
2024-06-05 00:16:41 +0200

    Finished 1st Test Case & added Failing Test 2

--- js/spec/gilded_rose_spec.js
+++ js/spec/gilded_rose_spec.js
@@ -1,7 +1,13 @@
 describe("Gilded Rose", function() {
   it("should decrease sell_in and quality by 1 for normal items", function() {
-
+    items = [ new Item("normal item", 10, 20) ];
+    update_quality();
     expect(items[0].sell_in).toEqual(9);
     expect(items[0].quality).toEqual(19);
+  });
+
+  it("should not decrease quality below 0", function() {
+
+    expect(items[0].quality).toEqual(0);
   });
 });
